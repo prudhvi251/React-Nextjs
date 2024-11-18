843395da5c40d598d7c37b7a89172f6ac971c411
prudhvi251 <prudhvimyadam@gmail.com>
2024-11-18 04:01:38 -0800

    Initial commit - React + Next Js  project

---
+++ src/app/hotels/HotelBlock.js
@@ -0,0 +1,16 @@
+"use client";
+
+import Image from "next/image";
+
+export default function HotelBlock({id, name, capacity}){
+    const imageLoader = ({src}) =>{
+        return `./Hotels/${src}.jpg`
+    };
+    return(
+<div>
+    <h2>{name}</h2>
+    <p>{capacity}</p>
+    <Image src={id} width={300} height={300} loader={imageLoader} alt="x" />
+</div>
+    );
+}
---
+++ src/app/hotels/page.js
@@ -0,0 +1,24 @@
+import HotelBlock from "./hotelBlock";
+
+async function getData()
+{
+    const res = await fetch("https://snowtooth-hotel-api.fly.dev")
+    return res.json();
+}
+
+
+
+
+export default async function Page(){
+    const data = await getData()
+    return(
+        <main>
+            <h1>Hotel Details</h1>
+            <div>
+            {data.map((hotel)=> (
+                <HotelBlock key={hotel.id} id={hotel.id} name={hotel.name} capacity={hotel.capacity}/>
+            ))}
+            </div>
+        </main>
+    )
+}
